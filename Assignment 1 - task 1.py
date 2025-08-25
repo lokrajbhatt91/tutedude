aed5ebb9f512093b02c94eaa9767f707a0564c12
lokrajbhatt91 <lokrajbhatt91@gmail.com>
2025-08-25 12:08:31 +0530

    Add files via upload

---
+++ Assignment 1 - task 1.py
@@ -0,0 +1,9 @@
+'''Assignment 1 task -1'''
+
+x=int(input("Enter the first number: "))
+y=int(input("Enter the second number: "))
+
+print("Addition: ",x+y)
+print("Subtraction: ",x-y)
+print("Multiplication: ",x*y)
+print("Division: ",x/y)
---
+++ Assignment 1 - task 2.py
@@ -0,0 +1,5 @@
+'''Assignment 1 - task 2'''
+first=input("Enter your first name: ")
+last=input("Enter your last name: ")
+
+print(f"Hello, {first} {last}! Welcome to the Python program.")

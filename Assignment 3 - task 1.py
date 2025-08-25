aa1a2006ec986921e0655c8b5e108cb5599c6758
lokrajbhatt91 <lokrajbhatt91@gmail.com>
2025-08-25 12:45:21 +0530

    Add files via upload

---
+++ Assignment 3 - task 1.py
@@ -0,0 +1,10 @@
+def factorial(num):
+    if(num <0):
+        return("Undefined")
+    if(num<=1):
+        return(1)
+    else:
+        return(num*factorial(num-1))
+        
+num=int(input("Enter a number: "))
+print(f"Factorial of {num} is: {factorial(num)}")
---
+++ Assignment 3 - task 2.py
@@ -0,0 +1,5 @@
+import math as mt
+x=int(input("Enter a number: "))
+print(f"Square root: {mt.sqrt(x)}")
+print(f"Logarithm: {mt.log(x)}")
+print(f"Sine: {mt.sin(x)}")

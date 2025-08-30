53f7edfe99e5b6e8cbb3c77da043bae18c737535
lokrajbhatt91 <lokrajbhatt91@gmail.com>
2025-08-30 09:59:29 +0530

    Add files via upload

---
+++ Assignment 4 - task 1.py
@@ -0,0 +1,13 @@
+'''Assignment 4 task - 1'''
+try:
+    text_file=open("sample.txt",'r')
+    print("Reading file content:")
+    line_number=len(text_file.readlines())
+    text_file.seek(0)
+    for line,x in zip(text_file,range(1,line_number+1)):
+        print(f"Line {x}: {line}",end="")
+    #print(text_file.read(}))
+except FileNotFoundError:
+    print("Error: The file \'sample.txt\' was not found.")
+finally:
+    text_file.close()

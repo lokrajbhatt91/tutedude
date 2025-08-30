4a204a56054379fc97ce7c3c1888ec095516b7d4
lokrajbhatt91 <lokrajbhatt91@gmail.com>
2025-08-30 10:13:16 +0530

    Update Assignment 4 - task 1.py

--- Assignment 4 - task 1.py
+++ Assignment 4 - task 1.py
@@ -1,12 +1,10 @@
-'''Assignment 4 task - 1'''
 try:
-    text_file=open("sample.txt",'r')
-    print("Reading file content:")
-    line_number=len(text_file.readlines())
-    text_file.seek(0)
-    for line,x in zip(text_file,range(1,line_number+1)):
-        print(f"Line {x}: {line}",end="")
-    #print(text_file.read(}))
-    text_file.close()
-except (FileNotFoundError, NameError):
-    print("Error: The file \'sample.txt\' was not found.")
+    with open("sample.txt", 'r') as text_file:
+        print("Reading file content:")
+        lines = text_file.readlines()
+        
+        for i, line in enumerate(lines, start=1):
+            print(f"Line {i}: {line}", end="")
+
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found.")
